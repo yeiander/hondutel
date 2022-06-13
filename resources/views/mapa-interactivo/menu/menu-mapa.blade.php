eea4fb33d7add6e1163c65381409c27eb58af52f
Franklin Acosta <frankacostahn@gmail.com>
2022-06-13 11:39:43 -0600

    Franklin  13/06/22

---
+++ resources/views/mapa-interactivo/menu/menu-mapa.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+  <section class="section">
+    <div class="section-header"  style="max-height: 3rem;">
+      <h5 class="page__heading">Menu</h5>
+    </div>
+        <div class="section-body">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="card">
+                <div class="card-body">
+
+    {{-------------------------- INICIO --------------------------}}
+                        
+              
+         
+       
+    {{-------------------------- FINAL ---------------------------}}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+@endsection
+
+
