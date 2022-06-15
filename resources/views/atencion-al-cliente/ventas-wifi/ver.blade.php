6b2460bd63f4bd92ca0843ff66f9a1676c36fc37
CarlosDiscua <discuacarlos17@gmail.com>
2022-06-15 15:14:56 -0600

    CarlosDiscua15/06/22

--- resources/views/atencion-al-cliente/ventas-wifi/ver.blade.php
+++ resources/views/atencion-al-cliente/ventas-wifi/ver.blade.php
@@ -21,44 +21,102 @@
                                     <div class="col-sm">
                                      {{-- columna1 inicio --}}
 
+                                      
+
                                      <div class="container">
                                       <div class="row">
                                         <div class="col-sm">
                                           <div class="col-xs-12 col-sm-12 col-md-12">
                                             <div class="form-inline">
                                              <div class="form-group">
-                                              <label id="rtn"><h6>R.T.N.:</h6></label>
-                                              <label id="rtn"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->rtn}} </h6></label>
+                                              <label id="id"><h6>Identidad:</h6></label>
+                                              <label id="id"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->id}} </h6></label>
                                              </div>
                                               </div>
-                                          </div>   
-            
+                                          </div>  
+                                          
+                                          <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-inline">
+                                          <div class="form-group">
+                                          <label id="rtn"><h6>R.T.N.:</h6></label>
+                                          <label id="rtn"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->rtn}} </h6></label>
+                                          </div>
+                                          </div>
+                                      </div>  
+                                          
+                                      <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-inline">
+                                          <div class="form-group">
+                                          <label id="clienteNombre"><h6>Nombre del cliente:</h6></label>
+                                          <label id="clienteNombre"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->clienteNombre}} </h6></label>
+                                          </div>
+                                          </div>
+                                      </div> 
+                                          
 
-       
-                                    <div class="col-xs-12 col-sm-12 col-md-12">
-                                      <div class="form-group">
-                                         <label style="font-size:16px; font-weight:bold; color:rgb(92, 92, 92)"  for="wifiTelefonoAsociado">Telefono Asociado:</label>
-                                         <input value="{{$registro->wifiTelefonoAsociado}}" class="form-control" type="text" name="wifiTelefonoAsociado" id="wifiTelefonoAsociado">
-                                       </div>
-                                    </div>
+                                          
+                                      <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-inline">
+                                          <div class="form-group">
+                                          <label id="wifiTelefonoAsociado"><h6>Telefono Asociado:</h6></label>
+                                          <label id="wifiTelefonoAsociado"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->wifiTelefonoAsociado}} </h6></label>
+                                          </div>
+                                          </div>
+                                      </div>   
 
-         
-                                    <div class="col-xs-12 col-sm-12 col-md-12">
-                                      <div class="form-group">
-                                         <label style="font-size:16px; font-weight:bold; color:rgb(92, 92, 92)"  for="clienteTelefonoOficina">Telefono de Oficina:</label>
-                                         <input value="{{$registro->clienteTelefonoOficina}}" class="form-control" type="text" name="clienteTelefonoOficina" id="clienteTelefonoOficina">
-                                       </div>
-                                    </div>
+                                      <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-inline">
+                                          <div class="form-group">
+                                          <label id="clienteTelefonoOficina"><h6>Telefono de Oficina:</h6></label>
+                                          <label id="clienteTelefonoOficina"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->clienteTelefonoOficina}} </h6></label>
+                                          </div>
+                                          </div>
+                                      </div> 
 
-                                    <div class="col-xs-12 col-sm-12 col-md-12">
-                                      <div class="form-group">
-                                         <label style="font-size:16px; font-weight:bold; color:rgb(92, 92, 92)"  for="propietarioLinea">Linea del propietario:</label>
-                                         <input value="{{$registro->propietarioLinea}}" class="form-control" type="text" name="propietarioLinea" id="propietarioLinea">
-                                       </div>
-                                    </div>
+                                      <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-inline">
+                                          <div class="form-group">
+                                          <label id="propietarioLinea"><h6>Linea del propietario:</h6></label>
+                                          <label id="propietarioLinea"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->propietarioLinea}} </h6></label>
+                                          </div>
+                                          </div>
+                                      </div> 
 
-                                    
-                                   
+                                      <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-inline">
+                                          <div class="form-group">
+                                          <label id="categorias"><h6>Categorias:</h6></label>
+                                          <label id="categorias"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->categorias}} </h6></label>
+                                          </div>
+                                          </div>
+                                      </div> 
+
+                                      <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-inline">
+                                          <div class="form-group">
+                                          <label id="fechaSolicitud"><h6>Fecha de Solicitud:</h6></label>
+                                          <label id="fechaSolicitud"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->fechaSolicitud}} </h6></label>
+                                          </div>
+                                          </div>
+                                      </div>
+
+                                      <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-inline">
+                                          <div class="form-group">
+                                          <label id="nombrePropietario"><h6>Nombre del propietario:</h6></label>
+                                          <label id="nombrePropietario"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->nombrePropietario}} </h6></label>
+                                          </div>
+                                          </div>
+                                      </div>
+
+                                      <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-inline">
+                                          <div class="form-group">
+                                          <label id="equipoInstalacion"><h6>Equipo de instalacion:</h6></label>
+                                          <label id="equipoInstalacion"><h6 style="color: blue; margin-left:0.2rem;">  {{$registro->equipoInstalacion}} </h6></label>
+                                          </div>
+                                          </div>
+                                      </div>
                                     
 
                                     
@@ -68,38 +126,9 @@
 
                                     <div class="col-sm">
                                       {{-- columna2 inicio --}}
+                                      
+                                      
 
-                                    <div class="col-xs-12 col-sm-12 col-md-12">
-                                      <div class="form-group">
-                                         <label style="font-size:16px; font-weight:bold; color:rgb(92, 92, 92)"  for="categorias">Categorias:</label>
-                                         <input value="{{$registro->categorias}}" class="form-control" type="text" name="categorias" id="categorias">
-                                       </div>
-                                    </div>
-
-                                    <div class="col-xs-12 col-sm-12 col-md-12">
-                                      <div class="form-group">
-                                         <label style="font-size:16px; font-weight:bold; color:rgb(92, 92, 92)"  for="fechaSolicitud">Fecha de Solicitud:</label>
-                                         <input value="{{$registro->fechaSolicitud}}" class="form-control" type="date" name="fechaSolicitud" id="fechaSolicitud">
-                                       </div>
-                                    </div>
-
-                                    <div class="col-xs-12 col-sm-12 col-md-12">
-                                      <div class="form-group">
-                                         <label style="font-size:16px; font-weight:bold; color:rgb(92, 92, 92)"  for="nombrePropietario">Nombre del propietario:</label>
-                                         <input value="{{$registro->nombrePropietario}}" class="form-control" type="text" name="nombrePropietario" id="nombrePropietario">
-                                       </div>
-                                    </div>
-
-                                    <div class="col-xs-12 col-sm-12 col-md-12">
-                                      <div class="form-group">
-                                         <label style="font-size:16px; font-weight:bold; color:rgb(92, 92, 92)"  for="equipoInstalacion">Equipo de instalacion:</label>
-                                         <input value="{{$registro->equipoInstalacion}}" class="form-control" type="text" name="equipoInstalacion" id="equipoInstalacion">
-                                       </div>
-                                    </div>
-
-    
-
-                                   
                                      {{-- coloumna2 final --}}
                                     </div>
                                 </div>       
