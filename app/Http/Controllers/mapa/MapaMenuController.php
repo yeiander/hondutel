fc4bbd1e7a1759115a262404124906b5dba7d2ea
Franklin Acosta <frankacostahn@gmail.com>
2022-06-17 10:51:18 -0600

    Franklin 17/06/22

--- app/Http/Controllers/mapa/MapaMenuController.php
+++ app/Http/Controllers/mapa/MapaMenuController.php
@@ -11,4 +11,12 @@ class MapaMenuController extends Controller
     public function menuMapa(){
     return View('/mapa-interactivo/mapa-menu');
     }
+
+    public function vistaMapa(){
+        return View('/mapa-interactivo/mapa');
+        }
+        public function menuCrearCoordenadas(){
+            return View('/mapa-interactivo/menu-crear-coordenadas');
+            }
+        
 }
