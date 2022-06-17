a82f7e23c1c16539f832fa98c523c1e77cdc3224
CarlosDiscua <discuacarlos17@gmail.com>
2022-06-17 09:05:21 -0600

    CarlosDiscua17/06/22

--- resources/views/atencion-al-cliente/menu.blade.php
+++ resources/views/atencion-al-cliente/menu.blade.php
@@ -8,7 +8,12 @@
         <div class="section-body">
             <div class="row">
                 <div class="col-lg-12">
+                  <button type="button" class="btn btn-primary">Etapas 1</button>
+                  <button type="button" class="btn btn-primary">Etapas 2</button>
+                  <button type="button" class="btn btn-primary">Etapas 3</button>
+                  <button type="button" class="btn btn-primary">Etapas 4</button>
                     <div class="card">
+                      
                         <div class="card-body">
                             {{-- inicio --}}
                             <div class="container">
