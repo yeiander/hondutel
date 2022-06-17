1c4a13848bdd131b134efe8fc8785011501871fd
yeiander <yeiandersonfl@gmail.com>
2022-06-17 15:19:32 -0600

    Yeison 17/6/2022

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,51 +1,28 @@
 <li  class="side-menus {{ Request::is('*') ? 'active' : 'inactive' }}">
     
     <a class="nav-link" href="{{ route('home')}}">
-        <i class=" fas fa-building"></i><span>Inicio</span>
+        <i class=" fas fa-building"></i><span>INICIO</span>
     </a>
 
-    
     <a class="nav-link" href="{{ route('usuarios.index')}}">
-        <i class=" fas fa-users"></i><span>Usuarios</span>
+        <i class=" fas fa-users"></i><span>USUARIOS</span>
     </a>
    
-    
-    
     <a class="nav-link" href="{{ route('roles.index')}}">
-        <i class=" fas fa-user-lock"></i><span>Roles</span>
+        <i class=" fas fa-user-lock"></i><span>ROLES</span>
     </a>
    
-{{--     
     <a class="nav-link" href="{{ route('recursos_humanos')}}">
-        <i class=""><img class="ml-2 mr-4" src="{{ asset('img/grupo.png') }}" height="30px"></i><span>Recursos Humanos</span>
-    </a> --}}
-
-    <a class="nav-link" href="{{ route('recursos_humanos')}}">
-        <i class=" fas fa-house-user"></i><span>Recursos Humanos</span>
+        <i class=" fas fa-house-user"></i><span>RECURSOS HUMANOS</span>
     </a>
-
-
-   
-    {{-- <a class="nav-link" href="{{ route('menu')}}">
-        <i class=""><img class="ml-2 mr-4" src="{{ asset('img/atencion al cliente.png') }}" height="30px"></i><span>Atencion al Cliente</span>
-    </a> --}}
 
     <a class="nav-link" href="{{ route('menu')}}">
-        <i class=" fas fa-phone-square"></i><span>Atencion al cliente</span>
+        <i class=" fas fa-phone-square"></i><span>ATENCION AL CLIENTE</span>
     </a>
     
-
-    {{-- <a class="nav-link" href="{{ route('mapa-menu')}}">
-        <i class=""><img class="ml-2 mr-4" src="{{ asset('img/mapa.png') }}" height="30px"></i><span>Mapa Interactivo</span>
-    </a> --}}
-
-
     <a class="nav-link" href="{{ route('mapa-menu')}}">
-        <i class=" fas fa-map"></i><span>Mapa Interactivo</span>
+        <i class=" fas fa-map"></i><span>MAPA INTERACTIVO</span>
     </a>
-    {{-- mi prueba --}}
- 
-     
-      {{-- aqui terminara mi prueba --}}
+   
     
 </li>
